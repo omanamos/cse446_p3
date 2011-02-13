22b1bd85569cb41efb027b5c21ecc9a40534d3be
omanamos <omanamos@gmail.com>
2011-02-13 10:10:29 -0800

    pos

--- src/Document.java
+++ src/Document.java
@@ -1,21 +1,41 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
 
 public class Document implements Iterable<String> {
-	
+	private enum Rule {NORM, POS};
+	private static final Rule TYPE = Rule.POS;
 	/**
 	 * word -> number of times the given word occurs in this document
 	 */
 	private Map<String, Integer> counts;
 	
 	public Document(File f){
+		switch(TYPE){
+		case NORM:
+			this.counts = buildNorm(f);
+		case POS:
+			this.counts = buildPos(f);
+		}
+	}
+	
+	private static Map<String, Integer> buildNorm(File f){
+		Map<String, Integer> rtn = new HashMap<String, Integer>();
 		try {
-			this.counts = new HashMap<String, Integer>();
 			Scanner s = new Scanner(f);
 			boolean isHeader = true;
 			
@@ -28,9 +48,9 @@ public class Document implements Iterable<String> {
 					for(String t : tokens){
 						t = t.trim().toLowerCase(); //remove whitespace and lower-case word
 						if(t.length() != 0){ //don't add empty strings
-							if(!counts.containsKey(t))
-								counts.put(t, 0);
-							counts.put(t, counts.get(t) + 1);
+							if(!rtn.containsKey(t))
+								rtn.put(t, 0);
+							rtn.put(t, rtn.get(t) + 1);
 						}
 					}
 				}
@@ -38,6 +58,28 @@ public class Document implements Iterable<String> {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		return rtn;
+	}
+	
+	private static Map<String, Integer> buildPos(File f){
+		Map<String, Integer> rtn = new HashMap<String, Integer>();
+		try{
+			MaxentTagger tagger = new MaxentTagger("pos_tagger/models/left3words-wsj-0-18.tagger");
+		    @SuppressWarnings("unchecked")
+		    List<ArrayList<? extends HasWord>> sentences = tagger.tokenizeText(new BufferedReader(new FileReader(f)));
+		    for (ArrayList<? extends HasWord> sentence : sentences) {
+		    	ArrayList<TaggedWord> tSentence = tagger.tagSentence(sentence);
+		    	for(TaggedWord w : tSentence){
+		    		String taggedWord = w.word() + "_" + w.tag();
+		    		if(!rtn.containsKey(taggedWord))
+		    			rtn.put(taggedWord, 0);
+		    		rtn.put(taggedWord, rtn.get(taggedWord) + 1);
+		    	}
+		    }
+		}catch(Exception e){
+			 e.printStackTrace();
+		 }
+		return rtn;
 	}
 	
 	/**
